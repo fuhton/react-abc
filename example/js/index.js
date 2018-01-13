5d0d6cf3bfda26e341381ea58a7a41e99b1283b5
Nicholas Smith <nicholasparkersmith@gmail.com>
2018-01-12 22:56:55 -0500

    fixing routing

--- example/js/index.js
+++ example/js/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // eslint-disable-line
+import { HashRouter } from 'react-router-dom'; // eslint-disable-line
 import App from './App';
 
 render(
   (
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   ), document.getElementById('root'),
 );
